a28705fe331723afd0d3d8175b28744c1da57be2
Rohith C <rohithc.eec.cseb@gmail.com>
2023-07-01 14:41:12 +0530

    Explanation

--- dynamic programming/Removal-game.cpp
+++ dynamic programming/Removal-game.cpp
@@ -618,6 +618,19 @@ void solve(){
 // }
 
 //performing optimization
+
+// 4,5,1,3
+// calculating using dp starting from the given below elements
+// 3
+// 1
+// 5
+// 4
+// 1,3
+// 5,1
+// 4,5
+// 5,1,3
+// 4,5,1
+// 4,5,1,3
    while(row>=0){
     int i=row;
     int j=col;  
